340d323084efd626661baabf00452411c5d39fe0
DrBubblez <harrisonminer59@gmail.comgit config --global uder.name DrBubblezgit config --global user.email harrisonminer59@gmail.com>
2024-03-12 14:27:40 -0400

    all clear button with clear function

--- assets/script.js
+++ assets/script.js
@@ -90,3 +90,8 @@ equalsButton.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
 })
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
