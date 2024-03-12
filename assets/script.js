bf24afc70d33672a2e5719a883fff784a79ab598
DrBubblez <harrisonminer59@gmail.comgit config --global uder.name DrBubblezgit config --global user.email harrisonminer59@gmail.com>
2024-03-12 15:00:58 -0400

    clear previous display after calculation

--- assets/script.js
+++ assets/script.js
@@ -80,6 +80,8 @@ class Calculator {
         if (this.operation != null) {
             this.previousOperandTextElement.innerText = 
                 `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
         }
     }
 }
