708f5b100b3fddb08a848ef0bee67e4ac5bdeb00
DrBubblez <harrisonminer59@gmail.comgit config --global uder.name DrBubblezgit config --global user.email harrisonminer59@gmail.com>
2024-03-12 14:31:20 -0400

    delete button and function

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
@@ -93,5 +93,10 @@ equalsButton.addEventListener('click', button => {
 
 allClearButton.addEventListener('click', button => {
     calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
     calculator.updateDisplay()
 })
